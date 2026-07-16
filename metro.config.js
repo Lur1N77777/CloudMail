4e9d4dfae8ce21de52253ffeac583cc44e32b8b0
Lur1N77777 <linminheng2518@gmail.com>
2026-07-16 10:39:15 +0800

    ci: use NativeWind virtual CSS modules on GitHub

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,7 @@ const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
+  // Keep the iOS development workaround, but let GitHub's Linux runner use
+  // NativeWind's virtual module patch so Metro can hash generated web CSS.
+  forceWriteFileSystem: process.env.GITHUB_ACTIONS !== "true",
 });
